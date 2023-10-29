4541cdcaf20d3178997faa567f60a8867dc9b52d
YKFire <102524725+YKFire@users.noreply.github.com>
2023-10-29 23:53:20 +0800

    add

--- test.py
+++ test.py
@@ -10,11 +10,13 @@ api_secret = ""   #填写控制台中获取的 APISecret 信息
 api_key =""    #填写控制台中获取的 APIKey 信息
 
 #用于配置大模型版本，默认“general/generalv2”
-domain = "general"   # v1.5版本
+# domain = "general"   # v1.5版本
 # domain = "generalv2"    # v2.0版本
+domain = "generalv3"
 #云端环境的服务地址
-Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
+# Spark_url = "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
 # Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
+Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat"
 
 
 text =[]
